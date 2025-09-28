1b2852749c2aac53735b4e128e8a5a0ed8708267
pufu <putra.azzam404@smp.belajar.id>
2025-09-28 10:55:16 +0700

    Add files via upload

---
+++ 1. Pufutara Full Web/proses.php
@@ -0,0 +1,10 @@
+<?php
+// Ambil data dari form
+$nama = $_POST['nama'];
+$tanya = $_POST['tanya'];
+
+// Tampilkan data
+echo "<h2>Data yang kami dapatkan :</h2>";
+echo "Nama kamu : $nama<br>";
+echo "Yang kamu tulis : $tanya<br>";
+?>
